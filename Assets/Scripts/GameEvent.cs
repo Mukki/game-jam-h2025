573412b11f02f86ef0e83e27305b63f0d52ba1e4
Jp <turpi0496@gmail.com>
2025-03-01 14:49:09 -0500

    Add GameEvents with 1 or 2 params

--- Assets/Scripts/GameEvent.cs
+++ Assets/Scripts/GameEvent.cs
@@ -54,6 +54,92 @@ public class GameEvent : Singleton<GameEvent>
     }
 }
 
+public class GameEvent<T> : Singleton<GameEvent<T>>
+{
+    readonly IDictionary<Event, Action<T>> _listeners = new SortedDictionary<Event, Action<T>>();
+
+    public static void Register(Event eventId, Action<T> callback) => Instance.RegisterImpl(eventId, callback);
+
+    public static void Unregister(Event eventId, Action<T> callback) => Instance.UnregisterImpl(eventId, callback);
+
+    public static void UnregisterAll() => Instance.UnregisterAllImpl();
+
+    public static void Call(Event eventId, T param) => Instance.CallImpl(eventId, param);
+
+    void RegisterImpl(Event eventId, Action<T> callback)
+    {
+        if (!_listeners.ContainsKey(eventId))
+        {
+            _listeners.Add(eventId, callback);
+        }
+        else
+        {
+            _listeners[eventId] += callback;
+        }
+    }
+
+    void UnregisterImpl(Event eventId, Action<T> callback)
+    {
+        if (_listeners.ContainsKey(eventId))
+        {
+            _listeners[eventId] -= callback;
+        }
+    }
+
+    void UnregisterAllImpl() => _listeners.Clear();
+
+    void CallImpl(Event eventId, T param)
+    {
+        if (_listeners.ContainsKey(eventId) && _listeners[eventId] != null)
+        {
+            _listeners[eventId](param);
+        }
+    }
+}
+
+public class GameEvent<T1, T2> : Singleton<GameEvent<T1, T2>>
+{
+    readonly IDictionary<Event, Action<T1, T2>> _listeners = new SortedDictionary<Event, Action<T1, T2>>();
+
+    public static void Register(Event eventId, Action<T1, T2> callback) => Instance.RegisterImpl(eventId, callback);
+
+    public static void Unregister(Event eventId, Action<T1, T2> callback) => Instance.UnregisterImpl(eventId, callback);
+
+    public static void UnregisterAll() => Instance.UnregisterAllImpl();
+
+    public static void Call(Event eventId, T1 param1, T2 param2) => Instance.CallImpl(eventId, param1, param2);
+
+    void RegisterImpl(Event eventId, Action<T1, T2> callback)
+    {
+        if (!_listeners.ContainsKey(eventId))
+        {
+            _listeners.Add(eventId, callback);
+        }
+        else
+        {
+            _listeners[eventId] += callback;
+        }
+    }
+
+    void UnregisterImpl(Event eventId, Action<T1, T2> callback)
+    {
+        if (_listeners.ContainsKey(eventId))
+        {
+            _listeners[eventId] -= callback;
+        }
+    }
+
+    void UnregisterAllImpl() => _listeners.Clear();
+
+    void CallImpl(Event eventId, T1 param1, T2 param2)
+    {
+        if (_listeners.ContainsKey(eventId) && _listeners[eventId] != null)
+        {
+            _listeners[eventId](param1, param2);
+        }
+    }
+}
+
 
 /*
 // This is the implementation for a single param of any type. Add a {T1,T2} 
@@ -72,31 +158,31 @@ public class GameEvent<T> : Singleton<GameEvent<T>>
 
     void RegisterImpl(Event eventId, Action<T> callback)
     {
-        if (!listeners.ContainsKey(eventId))
+        if (!_listeners.ContainsKey(eventId))
         {
-            listeners.Add(eventId, callback);
+            _listeners.Add(eventId, callback);
         }
         else
         {
-            listeners[eventId] += callback;
+            _listeners[eventId] += callback;
         }
     }
 
     void UnregisterImpl(Event eventId, Action<T> callback)
     {
-        if (listeners.ContainsKey(eventId))
+        if (_listeners.ContainsKey(eventId))
         {
-            listeners[eventId] -= callback;
+            _listeners[eventId] -= callback;
         }
     }
 
-    void UnregisterAllImpl() => listeners.Clear();
+    void UnregisterAllImpl() => _listeners.Clear();
 
     void CallImpl(Event eventId)
     {
-        if (listeners.ContainsKey(eventId) && listeners[eventId] != null)
+        if (_listeners.ContainsKey(eventId) && _listeners[eventId] != null)
         {
-            listeners[eventId]();
+            _listeners[eventId]();
         }
     }
 }
