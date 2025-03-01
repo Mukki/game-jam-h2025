12cb1912ce8c596ac75cebb1b8041f1785b05b7d
Ornithogwak <60271430+Ornithogwak@users.noreply.github.com>
2025-03-01 12:53:18 -0500

    Change button Indexxx

--- Assets/UI/LevelMenu/LevelMenuInterface.cs
+++ Assets/UI/LevelMenu/LevelMenuInterface.cs
@@ -8,11 +8,20 @@ public class LevelMenuInterface : MonoBehaviour
 
     private void OnEnable()
     {
+        int currentIndex = 0
         foreach (ConstructionBase construction in ConstructionManager.Instance.availableConstructions)
         {
             GameObject newButton = Instantiate(buttonPrefab, buttonParent.transform);
+            newButton.GetComponent<LevelMenuButton>().index = currentIndex;
             newButton.GetComponent<LevelMenuButton>().textButton.text = construction.Name;
             newButton.GetComponent<LevelMenuButton>().displayedImage.sprite = construction.Image; 
+            newButton.GetComponent<Button>().onClick.AddListener(() => SelectPower(currentIndex));
+            currentIndex++;
         }
+    }
+
+    private void SelectPower(int currentIndex)
+    {
+        ConstructionManager.currentConstruction = currentIndex;
     }
 }
