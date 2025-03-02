d9076dd23cf5d6cb50e3a4a3ba95b701ec33ba78
Jp <turpi0496@gmail.com>
2025-03-01 20:14:03 -0500

    Add daily info on animals

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,11 +4,13 @@ using System.Collections;
 
 public class GameManager : Singleton<GameManager>
 {
-    public float money;
-    public float lenghtOfDay;
-    public float lenghtOfNight;
+    public float Money;
+    public float LenghtOfDay;
+    public float LenghtOfNight;
 
-    public bool stMethodeIsASecteurOfStFelicien = true;
+    public bool StMethodeIsASecteurOfStFelicien = true;
+
+    public int CurrentDay = 1;
 
     private IEnumerator couroutine;
 
@@ -16,14 +18,14 @@ public class GameManager : Singleton<GameManager>
     {
         base.OnAwake();
 
-        money = 100.0f;
-        lenghtOfDay = 20.0f;
-        lenghtOfNight = 40.0f;
+        Money = 100.0f;
+        LenghtOfDay = 20.0f;
+        LenghtOfNight = 40.0f;
     }
 
     public void StartOfDay()
     {
-        couroutine = DayCycleCountDown(lenghtOfDay);
+        couroutine = DayCycleCountDown(LenghtOfDay);
         StartCoroutine(couroutine);
     }
 
@@ -34,7 +36,7 @@ public class GameManager : Singleton<GameManager>
 
     public void StartOfNight()
     {
-         couroutine = NightCycleCountDown(lenghtOfNight);
+         couroutine = NightCycleCountDown(LenghtOfNight);
          StartCoroutine(couroutine);
     }
 
@@ -42,6 +44,7 @@ public class GameManager : Singleton<GameManager>
     {
         // if AnimalManager.Instance.animalCount == 0 -> GameOver();
         // else StartOfDay();
+        CurrentDay++;
         StartOfDay();
     }
 
@@ -52,7 +55,7 @@ public class GameManager : Singleton<GameManager>
 
     public void PayMoney(float amount)
     {
-        money -= amount;
+        Money -= amount;
         GameEvent.Call(Event.MoneyChanged);
     }
 
